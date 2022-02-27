2566bc9e6b2cf2b4055eeb784dbc0b2141b4da86
wizard113 <yeong.rok.lee@gmail.com>
2022-02-27 16:50:47 +0900

    Add test case

---
+++ test.spec.js
@@ -0,0 +1,11 @@
+const { describe } = require("mocha");
+
+describe('Default Test Set', ()=>{
+  it('test should be passed', ()=>{
+    console.log('test1. passed')
+  });
+
+  it('test2 should be passed', ()=>{
+      console.log('test2 passed')
+  });
+} );
